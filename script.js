0ee7e707c969fd3758667482ba9dfeef59a5f53c
Muzaffar Nafees <77542915+MuzaffarNafees@users.noreply.github.com>
2022-04-10 02:18:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["I am CSE Student", "I love Developing things"],
+        strings: ["I am a CSE Student", "I love Developing things"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student at Jamia Hamdard"],
+        strings: ["a Student at Jamia Hamdard"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
